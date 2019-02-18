85fbf2e6ad3584534b4b173acce4fafff90c96f5
marco <marco_12333@hotmail.es>
2019-02-17 22:18:11 -0600

    agregando clase ciudad y clase cliente

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Cliente
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int telefono { get; set; }
+        public string direccion { get; set; }
+        public string ciudad { get; set; }
+      public Cliente cliente { get; set; }
+    }
+}
