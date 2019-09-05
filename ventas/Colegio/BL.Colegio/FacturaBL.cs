012d17d0ab4556fc9a5135da96e1103bb1bbd1d6
marco <marco_12333@hotmail.es>
2019-09-05 17:44:40 -0600

    Agregando FacturaBL, validacion

---
+++ ventas/Colegio/BL.Colegio/FacturaBL.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Colegio
+{
+     public class FacturaBL
+    {
+        Contexto _contexto;
+        
+        public BindingList<Factura> ListaFacturas { get; set; }
+
+        public FacturaBL()
+        {
+            _contexto = new Contexto();
+        }
+        public BindingList<Factura> ObtenerFacturas()
+        {
+            _contexto.Facturas.Include("FacturaDetalle").Load();
+            ListaFacturas = _contexto.Facturas.Local.ToBindingList();
+
+            return ListaFacturas;
+        }
+        public void AgregarFactura()
+        {
+            var nuevaFactura = new Factura();
+            _contexto.Facturas.Add(nuevaFactura);
+        }
+
+        public void AgregarFacturaDetalle(Factura factura)
+        {
+            if (factura != null)
+            {
+                var nuevoDetalle = new FacturaDetalle();
+                factura.FacturaDetalle.Add(nuevoDetalle);
+
+            }
+        }
+        public void RemoverFacturaDetalle(Factura factura, FacturaDetalle facturaDetalle)
+        {
+            if (factura != null && facturaDetalle != null)
+            {
+                factura.FacturaDetalle.Remove(facturaDetalle);
+            }
+        }
+
+        public void CancelarCambios()
+        {
+            foreach(var item in _contexto.ChangeTracker.Entries())
+            {
+                item.State = EntityState.Unchanged;
+                item.Reload();
+            }
+
+        }
+        public Resultado GuardarFactura(Factura factura)
+        {
+            var resultado = Validar(factura);
+            if (resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+
+            _contexto.SaveChanges();
+            resultado.Exitoso = true;
+            return resultado;
+        } 
+
+        public Resultado Validar(Factura factura)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+
+            if (factura == null)
+            {
+                resultado.Mensaje = "Agregue una Factura para guardar!";
+                resultado.Exitoso = false;
+
+                return resultado;
+            }
+
+            if (factura.Activo == false)
+            {
+                resultado.Mensaje = "La factura esta anulada y no se pueden hacer cambios!";
+                resultado.Exitoso = false;
+            }
+            if (factura.AlumnoId == 0)
+            {
+                resultado.Mensaje = "Seleccione un Alumno";
+                resultado.Exitoso = false;
+            }
+            if (factura.FacturaDetalle.Count == 0)
+            {
+                resultado.Mensaje = "Agregue Grado a la factura";
+                resultado.Exitoso = false;
+            }
+            foreach (var detalle in factura.FacturaDetalle)
+            {
+                if (detalle.GradoId == 0)
+                {
+                    resultado.Mensaje = "Seleccione Grado Valdo";
+                    resultado.Exitoso = false;
+                }
+            }
+            return resultado;
+        }
+
+
+        public void CalcularFactura(Factura factura)
+        {
+            if (factura != null)
+            {
+                double subtotal = 0;
+
+                foreach (var detalle in factura.FacturaDetalle)
+                {
+                    var producto = _contexto.Grados.Find(detalle.GradoId);
+                    if (producto != null)
+                    {
+                        detalle.Precio = producto.Precio;
+                        detalle.Total = detalle.Cantidad * producto.Precio;
+
+                        subtotal += detalle.Total;
+                    }
+                }
+                factura.Subtotal = subtotal;
+                factura.Impuesto = subtotal * 0.15;
+                factura.Total = subtotal + factura.Impuesto;
+
+            }
+        }
+
+    }
+    public class Factura
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int AlumnoId { get; set; }
+        public Alumno ALumno { get; set; }
+        public int Grado { get; set; }
+        public BindingList<FacturaDetalle> FacturaDetalle { get; set; }
+        public double Subtotal { get; set; }
+        public double Impuesto { get; set; }
+        public double Total { get; set; }
+        public bool Activo { get; set; }
+      
+
+        public Factura()
+        {
+            Fecha = DateTime.Now;
+            FacturaDetalle = new BindingList<FacturaDetalle>();
+            Activo = true;
+        }
+    }
+    public class FacturaDetalle
+    {
+        public int Id { get; set; }
+        public int GradoId { get; set; }
+        public Grado Grado { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }
+        public  double Total { get; set; }
+
+        public FacturaDetalle()
+        {
+            Cantidad = 1;
+        }
+    }
+
+}
---
+++ ventas/Colegio/Win.Colegio/FormFactura.cs
@@ -0,0 +1,130 @@
+﻿using BL.Colegio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Colegio
+{
+    public partial class FormFactura : Form
+    {
+
+        FacturaBL _facturaBL;
+        AlumnosBL _alumnosBL;
+        GradosBL _gradosBL;
+
+        public FormFactura()
+
+        {
+            InitializeComponent();
+
+            _facturaBL = new FacturaBL();
+            listaFacturasBindingSource.DataSource = _facturaBL.ObtenerFacturas();
+
+            _gradosBL = new GradosBL();
+            listaGradosBindingSource.DataSource = _gradosBL.ObtenerGrados();
+
+            _alumnosBL = new AlumnosBL();
+            listaAlumnosBindingSource.DataSource = _alumnosBL.ObtenerAlumnos();
+        }
+
+        private void alumnoIdLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void alumnoIdComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormFactura_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _facturaBL.AgregarFactura();
+            listaFacturasBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void listaFacturasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaFacturasBindingSource.EndEdit();
+
+            var factura = (Factura)listaFacturasBindingSource.Current;
+            var resultado = _facturaBL.GuardarFactura(factura);
+
+            if (resultado.Exitoso== true)
+            {
+                listaFacturasBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Factura Guardada");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarHabilitarBotones(true);
+            _facturaBL.CancelarCambios();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var factura = (Factura)listaFacturasBindingSource.Current;
+
+            _facturaBL.AgregarFacturaDetalle(factura);
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var factura = (Factura)listaFacturasBindingSource.Current;
+            var facturaDetalle = (FacturaDetalle)facturaDetalleBindingSource.Current;
+
+            _facturaBL.RemoverFacturaDetalle(factura, facturaDetalle);
+
+            DeshabilitarHabilitarBotones(false);               
+        }
+
+        private void facturaDetalleDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            e.ThrowException = false;
+        }
+
+        private void facturaDetalleDataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var factura = (Factura)listaFacturasBindingSource.Current;
+            _facturaBL.CalcularFactura(factura);
+
+            listaFacturasBindingSource.ResetBindings(false);
+
+        }
+    }
+}
